41ad4d80ca5f39f9bb7df252a6e4020962853ba9
ashish7shelke <ashish7shelke@gmail.com>
2026-01-02 01:21:52 +0530

    Thread synchronization using Mutex

---
+++ The_Linux_Programming_Interface_By_MichaelKerrisk/30_Threads_Synchronization/02_Thread_incr_mutex.c
@@ -0,0 +1,59 @@
+#include <pthread.h>
+#include "../tlpi_lib/tlpi_hdr.h"
+
+static int glob = 0;
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
+static void* threadFunc(void* arg)
+{
+    int loops = *((int *)arg);
+    int loc, j, s;
+
+    for(j = 0; j < loops; j++)
+    {
+        s = pthread_mutex_lock(&mtx);
+        if(s!=0)
+            errExitEN(s, "pthread_mutex_lock");
+        
+        loc = glob;
+        loc++;
+        glob = loc;
+
+        s = pthread_mutex_unlock(&mtx);
+        if(s!=0)
+            errExitEN(s, "pthread_mutex_unlock");
+    }
+
+    return NULL;
+}
+
+int main(int argc, char*argv[])
+{
+    pthread_t t1, t2;
+    int loops, s;
+
+    loops = (argc > 1) ? getInt(argv[1], GN_GT_0, "num_loops") : 10000000;
+
+    s = pthread_create(&t1, NULL, threadFunc, &loops);
+    if(s != 0)
+        errExitEN(s, "pthread_create");
+    
+    s = pthread_create(&t2, NULL, threadFunc, &loops);
+    if(s!=0)
+        errExitEN(s, "pthread_create");
+    
+    s = pthread_join(t1, NULL);
+    if(s != 0)
+        errExitEN(s, "pthread_join");
+    
+    s = pthread_join(t2, NULL);
+    if(s != 0)
+        errExitEN(s, "pthread_join");
+    
+    printf("glob = %d \n",glob);
+    exit(EXIT_SUCCESS);
+}
+
+/* 
+gcc -std=c11 -Wall -Wextra -Wno-unused-parameter -Wno-attributes -Wno-format-truncation -pedantic -I../tlpi_lib 02_Thread_incr_mutex.c ../tlpi_lib/error_functions.c ../tlpi_lib/get_num.c -pthread -o 02_Thread_incr_mutex
+*/
